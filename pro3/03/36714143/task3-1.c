2bd16c53a6cc1f9f34d34ffa6134d81ee23a4580
nyamu <sayuko.necko@gmail.com>
2025-10-17 19:02:58 +0900

    zonnzonn

---
+++ pro3/03/36714143/task3-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("整数を入力せよ。");
+    
+    printf("nyaaaaaaaaaaaaaaa");
+    scanf("%d", &num);
+    printf("%dの符号を反転した値は%dです。\n", +num, -num);
+
+    return 0;
+    
+}
