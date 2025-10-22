2441ed325df82e15a11357b4f8def8da69efecdc
neckoshirasu <sayuko.necko@gmail.com>
2025-10-22 16:43:48 +0900

    neckoshirasu

---
+++ 02/36714143/task2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("身長を入力せよ");
+    scanf("%d", &num);
+    double ans = (num - 100)* 0.9;
+    printf("標準体重は%.1fです。\n",ans );
+
+    return 0;
+    
+}
