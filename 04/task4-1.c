c10b016973026244fd026cb98a8981bf6246fd5d
nyamu <sayuko.necko@gmail.com>
2025-10-23 14:19:05 +0900

    nu

--- 04/task4-1.c
+++ 04/task4-1.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int num;
+    double num1, num2, num3;
 
-    printf("身長を入力せよ");
-    scanf("%d", &num);
-    double ans = (num - 100)* 0.9;
-    printf("標準体重は%.1fです。\n",ans );
+    //キーボードから入力
+    printf("3つの実数値を入力せよ\n");
+    printf("num1 = ");   scanf("%lf", &num1);
+    printf("num2 = ");   scanf("%lf", &num2);
+    printf("num3 = ");   scanf("%lf", &num3);
 
+
+    double max = num1; 
+    double min = num1;
+
+    if(num2 > max)
+        max = num2;
+    if(num3 > max)
+        max = num3;
+
+    if(num2 < min)
+        min = num2;
+    if(num3 < min)
+        min = num3;
+
+    printf("最大値は%fです。\n",max );
+    printf("最小値は%fです。\n",min );
     return 0;
     
 }
+
+
