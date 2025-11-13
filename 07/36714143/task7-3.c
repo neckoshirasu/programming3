95f0a3c751aaea2b82e332327026b602e7ca7438
nyamu <sayuko.necko@gmail.com>
2025-11-13 19:39:10 +0900

    07

--- 07/36714143/task7-3.c
+++ 07/36714143/task7-3.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
-#define NAME_LEN    64
-
-struct student{
-    char name [NAME_LEN];
-    int height;
-    double weight;
-};
-
 int main(void)
 {
-    struct student takao = {"Takao", 173};
-    
-    printf("氏名のアドレス = %p\n", &takao.name);
-    printf("身長のアドレス = %p\n", &takao.height);
-    printf("体重のアドレス = %p\n", &takao.weight);
-    
+    return 0;
 }
 
 
