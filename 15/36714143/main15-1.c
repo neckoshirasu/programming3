76bb64bd94b67b47922821d4a42412995146b47e
neckoshirasu <sayuko.necko@gmail.com>
2026-01-22 14:01:50 +0900

    finish git add .!

--- 15/36714143/main15-1.c
+++ 15/36714143/main15-1.c
@@ -2,7 +2,10 @@
 #include "task15-1.h"
 
 int main(void) {
-    int n = 400;  // 点数
+    // int n = 743;
+    int n;  // 点数
+    puts("データ点数");
+    scanf("%d",&n);
     write_data("36714143-1.dat", func1, n);
     write_data("36714143-2.dat", func2, n);
     write_data("36714143-3.dat", func3, n);
